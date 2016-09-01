8ce2b6db7ad22546b2b5f498d37977ed6bbec599
eqinox <eqinox@abv.bg>
2016-09-01 12:01:40 +0300

    Utils.cs uploaded

---
+++ JobSite/Classes/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobSite.Classes
+{
+    public static class Utils
+    {
+        public static string CutText(string text, int length = 100)
+        {
+            if (text != null && text.Length >= length)
+            {
+                return text.Substring(0, length) + "...";
+            }
+            else
+            {
+                return text;
+            }
+        }
+    }
+}
